a512c319842cfdb84db3d67bb64b2c3d3c743692
LiuWenYuan <15816141883@163.com>
2020-04-06 16:04:14 +0800

    PMDIBM/PMDL v4.6a Source

---
+++ pmdibm/assemble.bat
@@ -0,0 +1,12 @@
+optasm pmdl;
+optasm pmdibm;
+optasm mc;
+optasm mch;
+optasm efc;
+optasm pmp;
+optlink pmdl,pmdl.com;
+optlink pmdibm,pmdibm.com;
+optlink mc;
+optlink mch;
+optlink efc;
+optlink pmp,pmp.com;
